c22ecbbd121bf1eba890610726f37d281790c102
Mikita Buinavets <mikita.buinavets@arua.co>
2025-06-29 03:38:27 +0300

    singleton, factory, builder

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,13 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    // Write your code here!
+    private static ThreadSafeSingleton instance = new ThreadSafeSingleton();
+
+    synchronized static ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
+
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,46 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    private final Properties properties = new Properties();
+
+    @Override
+    public Connection createConnection() {
+        ClassLoader loader = H2ConnectionFactory.class.getClassLoader();
+        try (InputStream stream = loader.getResourceAsStream("h2database.properties")) {
+            if (stream == null) {
+                throw new FileNotFoundException();
+            }
+            properties.load(stream);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load h2database.properties", e);
+        }
+
+        String driver = properties.getProperty("jdbc_driver");
+        String url = properties.getProperty("db_url");
+        String password = properties.getProperty("password");
+        String name = properties.getProperty("user");
+
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.fillInStackTrace();
+        }
+
+        try {
+            return DriverManager.getConnection(url, name, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
